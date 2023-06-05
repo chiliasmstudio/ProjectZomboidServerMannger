1a2e2a4d5e330030d0f752e663b1c740219ebb15
paul <paul002706@gmail.com>
2023-06-05 20:25:47 +0800

    temp

--- src/main/java/com/chiliasmstudio/ProjectZomboidServerMannger/lib/Util/Discord/Emote.java
+++ src/main/java/com/chiliasmstudio/ProjectZomboidServerMannger/lib/Util/Discord/Emote.java
@@ -20,6 +20,8 @@ package com.chiliasmstudio.ProjectZomboidServerMannger.lib.Util.Discord;
 
 public class Emote {
     public static final String Logo_ProjectZomboid = "<:Logo_ProjectZomboid:1004025758664097933>";//<:pz:963361244436381716>
+
+    public static final String Logo_Phoenix = "<:Logo_Phoenix:1115229600155447296>";//<:Logo_Phoenix:1115229600155447296>
 }
 
 
